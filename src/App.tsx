70d8112e31e77aa0ec0d98982d5b30180294b7f1
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-07-28 19:49:26 -0700

    feat: :sparkles: Renderize all the compnents
    we introduced all components will contain the webstite

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import CodeSection from "./components/CodeSection";
+import { Row, Col } from "antd";
+import FormSection from "./components/FormSection";
+import ChatWidgetLocal from "./components/ChatWidgetLocal";
+import Badge from "./components/Badge";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Badge />
+      <HeroSection />
+      <Row className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center mb-20">
+        <Col span={13}>
+          <CodeSection />
+        </Col>
+        <Col span={11}>
+          <FormSection />
+        </Col>
+      </Row>
+      <HowItWorks />
+      <Footer />
+      <ChatWidgetLocal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
